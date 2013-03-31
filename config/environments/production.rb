ed65ef1c88e56cdf9243568ab644dfb5a08d5490
Miguel Alvaro Molina <alvaromolinac@gmail.com>
2013-03-31 16:04:00 -0400

    mejoras

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Blog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.assets.compile = true
+
+  config.assets.initialize_on_precompile=false
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -15,7 +16,7 @@ Blog::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
